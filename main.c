53aa538565455f9b502fd4a6820623ba907fd5eb
rodrigoo-r <2nw53j32643y@gmail.com>
2025-05-04 17:42:35 -0500

    Refactor main function to accept command-line arguments and remove unused code

--- main.c
+++ main.c
@@ -19,29 +19,11 @@
 #include "runtime/print/print.h"
 #include "runtime/read_line/read_line.h"
 
-int main(void) {
+int main(const int argc, const char **argv) {
     // Initialize the runtime
     init_runtime();
 
-    print("Please enter your name: ");
-    const String line = runtime_read_line();
-    print("Hello, ");
-    println(get_string(&line)->value->element);
 
-    // Create a new guard
-    HeapGuard *guard = alloc(sizeof(char) * 14);
-    str_copy("Hello, world!", guard->value->element);
-
-    HashMap* my_hash_map = create_hash_map();
-    insert_to_map(my_hash_map, "message", guard->value->element);
-
-    println(get_from_map(my_hash_map, "message"));
-    destroy_hash_map(my_hash_map);
-
-    // Drop the guard (Not necessary since init_runtime adds an atexit hook
-    // (heap_cleanup) that acts as a GC that cleans the heap on exit)
-    // ref_count is decremented to 0 at this point, hence it is freed
-    drop_guard(guard);
 
     return 0;
 }
