ccc43350c55dbfc760091a0d38b0960dfcbe4f73
rodrigoo-r <2nw53j32643y@gmail.com>
2025-05-04 17:57:47 -0500

    Add ANSI color definitions for terminal output

---
+++ ansi/ansi.h
@@ -0,0 +1,33 @@
+/*
+    The Fluent Programming Language
+    -----------------------------------------------------
+    This code is released under the GNU GPL v3 license.
+    For more information, please visit:
+    https://www.gnu.org/licenses/gpl-3.0.html
+    -----------------------------------------------------
+    Copyright (c) 2025 Rodrigo R. & All Fluent Contributors
+    This program comes with ABSOLUTELY NO WARRANTY.
+    For details type `fluent l`. This is free software,
+    and you are welcome to redistribute it under certain
+    conditions; type `fluent l -f` for details.
+*/
+
+//
+// Created by rodrigo on 5/4/25.
+//
+
+#ifndef ANSI_H
+#define ANSI_H
+
+const char *ANSI_Reset        = "\033[0m";
+const char *ANSI_BrightRed    = "\033[91m";
+const char *ANSI_BrightYellow = "\033[93m";
+const char *ANSI_BrightPurple = "\033[95m";
+const char *ANSI_BrightBlack  = "\033[90m";
+const char *ANSI_BoldBrightBlue   = "\033[1;94m";
+const char *ANSI_BoldBrightGreen  = "\033[1;92m";
+const char *ANSI_BoldBrightRed    = "\033[1;91m";
+const char *ANSI_BoldBrightYellow = "\033[1;93m";
+const char *ANSI_BoldBrightPurple = "\033[1;95m";
+
+#endif //ANSI_H
