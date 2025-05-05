ef8e0d92667fa0536013bbb205274a02ccc62e97
rodrigoo-r <2nw53j32643y@gmail.com>
2025-05-05 11:02:24 -0500

    Make scripts executable

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
-sh build.sh
+# Make sure the executable exists, if not build it
+if [ ! -f "./cmake-build-debug/fluenti" ]; then
+    echo "Executable not found, building..."
+    ./build.sh
+fi
+
 ./cmake-build-debug/fluenti "$@"
