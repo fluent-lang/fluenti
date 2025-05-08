879a619e1762822b316e6030c02d7d1130fd13eb
rodrigoo-r <2nw53j32643y@gmail.com>
2025-05-08 15:55:40 -0500

    Add error handling and help generation in main function

--- main.c
+++ main.c
@@ -12,9 +12,21 @@
     conditions; type `fluent l -f` for details.
 */
 
+// CLI information
+#ifndef PROGRAM_NAME
+#define PROGRAM_NAME "fluenti"
+#endif
+
+#ifndef PROGRAM_DESC
+#define PROGRAM_DESC "The Fluent Language Interpreter"
+#endif
+
+#include <stdlib.h>
+
 #include "cli/argv_impl.h"
 #include "cli/default/get_flags.h"
 #include "runtime/init_runtime.h"
+#include "util/help_generator/help_generator.h"
 
 int main(const int argc, const char **argv) {
     // Initialize the runtime
@@ -31,6 +43,16 @@ int main(const int argc, const char **argv) {
     // Parse the CLI arguments
     const HeapGuard *cli_guard = parse_argv(argc, argv, flags->value->element);
     const Argv *cli_argv = cli_guard->value->element;
+
+    // Handle errors
+    if (
+        cli_argv->success == FALSE ||
+        get_from_map(cli_argv->static_flags, "help") != NULL
+    )
+    {
+        generate_help(PROGRAM_NAME, PROGRAM_DESC, flags->value->element);
+        return 2;
+    }
 
     // Drop the flag guard to free it since we don't need it anymore
     drop_guard(flags);
