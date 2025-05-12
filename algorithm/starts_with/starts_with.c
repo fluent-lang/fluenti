b0cf35ae87f39125fc6633149464adb89898f93d
rodrigoo-r <2nw53j32643y@gmail.com>
2025-05-12 08:52:05 -0500

    Add starts_with function to check if a string starts with a given prefix

---
+++ algorithm/starts_with/starts_with.c
@@ -0,0 +1,44 @@
+/*
+    The Fluent Programming Language
+    -----------------------------------------------------
+    This code is released under the GNU GPL v3 license.
+    For more information, please visit:
+    https://www.gnu.org/licenses/gpl-3.0.html
+    -----------------------------------------------------
+    Copyright (c) 2025 Rodrigo R. & All Fluent Contributors
+    This program comes with ABSOLUTELY NO WARRANTY.
+    For details type `fluent l`. This is free software,
+    and you are welcome to redistribute it under certain
+    conditions; type `fluent l -f` for details.
+*/
+
+//
+// Created by rodrigo on 5/11/25.
+//
+
+#include "starts_with.h"
+
+bool starts_with(const char *str, const char *prefix)
+{
+    // Iterate over the prefix
+    while (*prefix != '\0')
+    {
+        // Check if we have a null terminator
+        if (*str == '\0')
+        {
+            return FALSE;
+        }
+
+        // Check if we have the same character
+        if (*str != *prefix)
+        {
+            return FALSE;
+        }
+
+        // Move to the next character
+        *prefix++;
+        *str++;
+    }
+
+    return TRUE;
+}
