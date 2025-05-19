d4b894f77a80924f9a711d6b1d84721d197ade4a
rodrigoo-r <2nw53j32643y@gmail.com>
2025-05-19 18:49:54 -0500

    Remove C codebase

--- build.sh
+++
@@ -1,1 +0,0 @@
-cmake --build cmake-build-debug --target fluenti -j 14
--- run.sh
+++
@@ -1,7 +0,0 @@
-# Make sure the executable exists, if not build it
-if [ ! -f "./cmake-build-debug/fluenti" ]; then
-    echo "Executable not found, building..."
-    ./build.sh
-fi
-
-./cmake-build-debug/fluenti "$@"
